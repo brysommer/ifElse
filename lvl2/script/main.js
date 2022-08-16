d22e1056710e2858f96fe8a34c73cde3e09331c6
brysommermac <brysommer1@gmail.com>
2022-08-17 01:33:57 +0300

    I am not happy, but lvl 3 I think comleate

--- lvl2/script/main.js
+++ lvl2/script/main.js
@@ -12,70 +12,57 @@ const run = () => {
 
     if(valueSit === 'keepcalm') {
         console.log('Буває й лінь поїсти');
-        result.insertAdjacentHTML('beforeend', '<span><br>Буває й лінь поїсти<br></span>');
         return;
     };
 
     if(valueSit === 'getup') {
         console.log('Так троимати');
-        result.insertAdjacentHTML('beforeend', '<span>Так тримати<br></span>');
     };
 
     if(valueRoom === 'bathroom') {
         console.log('Ви прийняли душ');
-        result.insertAdjacentHTML('beforeend', '<span>Ви прийняли душ<br></span>');
-
         return;
     };
 
     if(valueRoom === 'kitchen') {
         console.log('Продовжуйте в тому ж дусі');
-        result.insertAdjacentHTML('beforeend', '<span>Продовжуйте в тому ж дусі<br></span>');
     };
 
     if(valueRoom === 'toilet') {
         console.log('Сподіваюсь усі справи доведено до кінця');
-        result.insertAdjacentHTML('beforeend', '<span>Сподіваюсь усі справи доведено до кінця<br></span>');
         return;
     };
 
     if(valueRoom === 'badroom') {
         console.log('Солодких снів');
-        result.insertAdjacentHTML('beforeend', '<span>Солодких сні<br></span>');
         return;
     };
 
     if(valueFridge === 'close') {
         console.log('Дієта, справа серйозна');
-        result.insertAdjacentHTML('beforeend', '<span>Дієта, справа серйозна<br></span>');
         return;
     };
 
     if(valueFridge === 'open') {
         console.log('ВИ близькі до цілі');
-        result.insertAdjacentHTML('beforeend', '<span>ВИ близькі до цілі<br></span>');
     };
 
     if(valueFind === 'check2') {
         console.log('На вас напала миш');
-        result.insertAdjacentHTML('beforeend', '<span>На вас напала миш<br></span>');
         fight();
         return;
     };
 
     if(valueFind === 'check1') {
         console.log('Вітаємо');
-        result.insertAdjacentHTML('beforeend', '<span>Вітаємо<br></span>');
     };
 
     if(valueFind === 'check3') {
         console.log('Кефір, це не те що на разі хочеться');
-        result.insertAdjacentHTML('beforeend', '<span>Кефір, це не те що на разі хочеться<br></span>');
         return;
     }; 
-
-console.log('Ви добули ковбасу, смачного');  
-result.insertAdjacentHTML('beforeend', '<span>Ви добули ковбасу, смачного<br></span>');
+    
+console.log('Ви добули ковбасу, смачного');    
 };
 
 run();
@@ -90,14 +77,12 @@ const background = document.querySelector('.background');
 
 switcherON.addEventListener('click', (click) => {
     background.classList.add('sun');
-    console.log('Увімкнули світло');  
-    result.insertAdjacentHTML('beforeend', '<span>Увімкнули світло<br></span>');  
+    console.log('Увімкнули світло');    
 });
 
 switcherOFF.addEventListener('click', (click) => {
     background.classList.remove('sun');
-    console.log('Вимкнули світло'); 
-    result.insertAdjacentHTML('beforeend', '<span>Вимкнули світло<br></span>');
+    console.log('Вимкнули світло');    
 });
 
 const fight = () => {
@@ -106,7 +91,6 @@ const fight = () => {
     message.innerHTML = '<h1>На вас напала миш, захищайся</h1>'
     weapon.addEventListener('click', (punch) => {
         console.log('Використали зброю');
-        result.insertAdjacentHTML('beforeend', '<span>Використали зброю<br></span>');
         valueFind = 'check1';
     });
 }
